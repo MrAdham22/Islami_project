1e0b19949a05bd92ab2b744bd60a178f3696cc54
Adham <adhamelgendy94@.com>
2026-02-12 07:50:13 +0200

    Finished the Navigation Buttons

---
+++ app/src/main/java/com/example/islami_project/ui/theme/main/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.islami_project.ui.theme.main
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.islami_project.R
+import com.example.islami_project.ui.theme.main.fragments.hadith.HadithFragment
+import com.example.islami_project.ui.theme.main.fragments.quran.QuranFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigation: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        bottomNavigation = findViewById(R.id.navbar)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, QuranFragment())
+            .commit()
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+            R.id.quran_Tab -> {
+                showFragment(QuranFragment())
+            }
+            R.id.hadeth_Tab -> {
+                showFragment(HadithFragment())
+            }
+            R.id.sebha_Tab-> {
+                Toast.makeText(this, "Task", Toast.LENGTH_SHORT).show()
+                return@setOnItemSelectedListener false
+            }
+            R.id.radio_Tab -> {
+                Toast.makeText(this, "not impleneted yet", Toast.LENGTH_SHORT).show()
+                return@setOnItemSelectedListener false
+            }
+
+            }
+            return@setOnItemSelectedListener true
+
+        }
+    }
+    fun showFragment (fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/example/islami_project/ui/theme/main/fragments/quran/QuranFragment.kt
@@ -0,0 +1,20 @@
+package com.example.islami_project.ui.theme.main.fragments.quran
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.islami_project.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class QuranFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quran, container, false)
+    }
+}
